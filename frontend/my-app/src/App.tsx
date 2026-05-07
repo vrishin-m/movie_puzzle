2e263167466d7578b8ce64be9ae112871bfa618b
Vrishin M <vrishinmahadevan@gmail.com>
2026-05-07 18:56:08 +0530

    made basic frontend
    will use tailwind css in the end, after setting up backend stuff

--- frontend/my-app/src/App.tsx
+++ frontend/my-app/src/App.tsx
@@ -1,8 +1,22 @@
 import { useState } from 'react'
 import './App.css'
-var question = ""
+var question = "placeholder question potato tomato icecream thomas the train tung tung sahur"
+var answer = ""
+var num_attempts =0
+
+
 function App() {
-  
+ const [submitted_answer, set_submitted_answer] = useState("");
+ const [currentInput, setCurrentInput] = useState("");
+ 
+ const handleSubmit = () => {
+    set_submitted_answer(currentInput);
+    num_attempts++;
+    setCurrentInput("");
+  };
+
+
+
 
   return (
     <>
@@ -11,25 +25,35 @@ function App() {
       <br />
       <h2> Made by Gandalf</h2>
     </div>
+    
+    <div className="game_ui">
+      <br /> <br /> <br /> <br /> 
+      <h1> "{question}"</h1>
+      <br /><br /> <br /><br /><br /><br />
+      
+      <div className="submit">
+        <textarea 
+          value={currentInput}
+          onChange={(e) => setCurrentInput(e.target.value)}
+          placeholder="Write your guess here"
+        />
+      
+        <button onClick={handleSubmit}>Submit Answer</button>
+      </div>
 
-    <Game_ui />
+      <br />
+      
+      <p>Attempts: {num_attempts}</p>
+      {num_attempts >= 2 && <button className="hint_button">Get Hint</button>}
+
+    </div>
 
     
+
     </>
   )
 }
 
-function Game_ui(){
-  return(
-    <div className="game_ui">
-      <h2> {question}</h2>
 
-      
-      
-      
-      
-      
-    </div>
-  )
-}
+
 export default App
