2f0cafd53d2384169c5ef2ffa71cb7566d00bb43
vrishin-m <vrishinmahadevan@gmail.com>
2026-05-15 00:47:56 +0530

    fixed the leaderboard

--- frontend/my-app/auth.tsx
+++ frontend/my-app/auth.tsx
@@ -23,8 +23,9 @@ export function Auth({ onLogin }: { onLogin: (token: string) => void }) {
       alert(data.error || "Authentication failed");
     }
   };
-  Leaderboard() 
+ 
   return (
+    
     <div style={{ padding: '20px', textAlign: 'center' }}>
       <h2>{isSignUp ? 'Create Account' : 'Login'}</h2>
       <form onSubmit={handleSubmit}>
@@ -37,69 +38,12 @@ export function Auth({ onLogin }: { onLogin: (token: string) => void }) {
       </button>
     </div>
     
+    
   );
   
 }
 
 
-const Leaderboard = () => {
-
-  interface Puzzle {
-    id: string;
-    username: string;
-    total_score: number;
-  }
-
-
-  const [users, setUsers] = useState<Puzzle[]>([]); 
-
-  users.map(u => console.log(u.id));
-
-  const [loading, setLoading] = useState(true);
-  
-
-  useEffect(() => {
-    const fetchUsers = async () => {
-      
-        const response = await fetch('http://localhost:3000/api/leaderboard');
-        const data = await response.json();
-        setUsers(data); 
-      } 
-
-    fetchUsers();
-  }, []); 
-
-  if (loading) return <div>Loading leaderboard..</div>;
-
-
-  return (
-    <div style={{ padding: '20px' }}>
-      <h2>Leaderboard</h2>
-      {users.length === 0 ? (
-        <p>No users found.</p>
-      ) : (
-        <table  cellPadding="10" style={{ width: '100%', textAlign: 'left' }}>
-          <thead>
-            <tr>
-
-              <th>Name</th>
-              <th>Total Score</th>
-            </tr>
-          </thead>
-          <tbody>
-            {users.map((user) => (
-              <tr key={user.id}>
-                <td>{user.username}</td>
-                <td>{user.total_score}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      )}
-    </div>
-  );
-}
- 
                     
 
 
--- frontend/my-app/src/App.tsx
+++ frontend/my-app/src/App.tsx
@@ -24,7 +24,34 @@ function App() {
 
 
 
+  interface Puzzle {
+    id: string;
+    username: string;
+    total_score: number;
+  }
 
+
+  const [users, setUsers] = useState<Puzzle[]>([]); 
+
+  users.map(u => console.log(u.id));
+
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    const fetchUsers = async () => {
+      
+        const response = await fetch('http://localhost:3000/api/leaderboard');
+        const data = await response.json();
+        setUsers(data); 
+      } 
+
+    fetchUsers();
+  }, []); 
+
+
+
+
+
+ 
 
 
 
@@ -202,6 +229,30 @@ function App() {
       )}
 
     </div>
+    <div style={{ padding: '20px' }}>
+      <h2>Leaderboard</h2>
+      {users.length === 0 ? (
+        <p>No users found.</p>
+      ) : (
+        <table  cellPadding="10" style={{ width: '100%', textAlign: 'left' }}>
+          <thead>
+            <tr>
+
+              <th>Name</th>
+              <th>Total Score</th>
+            </tr>
+          </thead>
+          <tbody>
+            {users.map((user) => (
+              <tr key={user.id}>
+                <td>{user.username}</td>
+                <td>{user.total_score}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
+    </div>
 
     </>
   )
@@ -213,3 +264,9 @@ function App() {
 
 
 export default App
+
+
+
+
+
+  
